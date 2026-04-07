1670e3a8aa9bf192ec4125fd25f4e470cf634dee
CodeAriCos28 <arafaelacosta28@gmail.com>
2026-04-06 21:42:34 -0400

    refactor: limpiar configuraciones innecesarias y mejorar la carga de variables de entorno en settings.py

--- restaurante/restaurante/settings.py
+++ restaurante/restaurante/settings.py
@@ -19,40 +19,30 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Carga las variables de entorno desde el archivo .env
 load_dotenv()
-# load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = 'django-insecure-m8))quru4uo6-(e#rgth5p5$1qo*f)h9h&h23tdmxhiu-77udo'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-# DEBUG = os.getenv("DEBUG", "False") == "True"
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-#
-# ALLOWED_HOSTS = ['cottaged-willy-delorse.ngrok-free.dev', 'localhost', '127.0.0.1']
 
 # SECRET_KEY = 'django-insecure-m8))quru4uo6-(e#rgth5p5$1qo*f)h9h&h23tdmxhiu-77udo'
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-#
-# ALLOWED_HOSTS = ['cottaged-willy-delorse.ngrok-free.dev', 'localhost', '127.0.0.1']
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = ['https://65.181.125.215','http://127.0.0.1','http://localhost']
+# SECURITY WARNING: don't run with debug turned on in production!
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
+# Agrega esta línea para configurar los orígenes confiables para CSRF
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'facturacion',
     'django.contrib.admin',
@@ -64,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
 ]
 
+# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -76,6 +67,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'restaurante.urls'
 
 TEMPLATES = [
@@ -99,9 +91,7 @@ WSGI_APPLICATION = 'restaurante.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
-
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         # 'ENGINE': 'django.db.backends.postgresql',
@@ -113,33 +103,8 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", # esto es para mysql
         }
-
     }
 }
-
-
-
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "restaurante",
-#         "USER": "root",
-#         "PASSWORD": "",
-#         "HOST": "localhost",
-#         "PORT": "3306",
-#         "OPTIONS": {
-#             # Usar offset numérico en lugar de nombre de zona
-#             "init_command": "SET time_zone = '-04:00'",  # Para República Dominicana (UTC-4)
-#             # Otras opciones importantes
-#             "charset": "utf8mb4",
-#             "use_unicode": True,
-#         },
-#         # Esto es crucial para MySQL con Django
-        
-#     }
-# }
-
 
 
 
@@ -166,11 +131,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"   # aquí se copiará todo
-
 
 #
 # MEDIA_URL = '/media/'
@@ -181,21 +146,16 @@ STATIC_ROOT = BASE_DIR / "staticfiles"   # aquí se copiará todo
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-TIME_ZONE = 'America/Santo_Domingo'  # Mantener para Django
+# TIME_ZONE = 'America/Santo_Domingo'  # Mantener para Django
 
 LANGUAGE_CODE = 'en-us'
 
-# TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-
-#STATIC_URL = 'static/'
 
 
